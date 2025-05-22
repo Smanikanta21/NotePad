81210e6ae2f192bea5fb288bd79d040896516113
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-05-22 19:42:41 +0530

    feat: add routing and navigation components with lucide-react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Nav from './components/Nav'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-      <Nav />
-    </>
+    <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path='/home' element={<div>Home</div>}/>
+        <Route path='/about' element={<div>About</div>}/>
+        <Route path='/contact' element={<div>Contact</div>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-
+import { React, useState } from 'react'
+import { LucideMenu } from 'lucide-react'
+import { Link } from 'react-router-dom'
 const Nav = () => {
+    const[showMenu,SetShowMenu]=useState(false)
   return (
-    <div className='items-center justify-center flex mt-2'>
-        <div className='flex w-[90%] flex-row items-center justify-between z-10 text-3xl px-4 py-2 rounded-3xl shadow-2xl'>
-            <div><a href="#">NotePad</a></div>
-            <div className='flex gap-4'>
-                <a href="#">Home</a>
-                <a href="#">Contact</a>
-                <a href="#">About</a>
+    <div className='items-center justify-center flex mt-2 relative'>
+        <div className='md:flex text-2xl font-medium justify-center items-center w-[90%] md:flex-row md:items-center md:justify-between z-10 md:text-3xl px-4 py-2 rounded-3xl shadow-2xl'>
+            <div className='flex justify-between items-center'>
+                <div className='md:hidden relative'><button onClick={()=>SetShowMenu(!showMenu)}><LucideMenu/></button></div>
+                <div><Link to="/">NotePad</Link></div>
+                <div></div>
+            </div>
+            <div className='md:flex hidden md:gap-4'>
+                <Link to="/home">Home</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/about">About</Link>
             </div>
         </div>
+        {showMenu && (
+          <div className="absolute top-20 left-4 right-4 md:hidden bg-white p-4 rounded-xl shadow-lg z-50 flex flex-col gap-2">
+            <Link to="/home">Home</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About</Link>
+          </div>
+        )}
     </div>
   )
 }
