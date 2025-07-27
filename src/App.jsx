e52b3900ebd6090916272e822d02c5f5117ad65c
Smanikanta72 <siraparapuabhinay21@gmail.com>
2025-07-27 20:05:20 +0530

    Added Toasitfy and moved browser router from main.jsx to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Home from './components/Home'
 import Login from './components/LoginPage'
 import Signup from './components/SignupPage'
 import LandingPage from './components/LandingPage'
-import { ToastContainer } from 'react-toastify'
+import ToastContainer from 'react-toastify'
 import Setting from './components/settings'
 import Profile from './components/Profile'
 import { BrowserRouter,Routes,Route,Outlet } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
 
   return (
     <>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<Login />} />
@@ -24,6 +26,8 @@ function App() {
         </Route>
       </Routes>
       <ToastContainer position="top-right" autoClose={3000} />
+    </BrowserRouter>
+
     </>
   )
 }
